dc688c73b8bd3b5d3e19b19426d930a5e8bc33ed
Roxana González <roxana@fituapp.com>
2020-05-14 10:17:58 -0600

    able to indicate ux_mode redirect and able to set redirect_uri

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ var googleAuth = (function () {
 
   function initClient(config) {
     return new Promise((resolve, reject) => {
+      console.log(config)
       window.gapi.load('auth2', () => {
         window.gapi.auth2.init(config)
           .then(() => {
@@ -38,13 +39,14 @@ var googleAuth = (function () {
     this.isAuthorized = false
     this.isInit = false
     this.prompt = null
+    this.ux_mode = 'redirect'
     this.isLoaded = function () {
       /* eslint-disable */
       console.warn('isLoaded() will be deprecated. You can use "this.$gAuth.isInit"')
       return !!this.GoogleAuth
     };
 
-    this.load = (config, prompt) => {
+    this.load = (config, prompt, ux_mode) => {
       installClient()
         .then(() => {
           return initClient(config)
@@ -53,6 +55,7 @@ var googleAuth = (function () {
           this.GoogleAuth = gapi.auth2.getAuthInstance()
           this.isInit = true
           this.prompt = prompt
+          this.ux_mode = ux_mode //'redirect'
           this.isAuthorized = this.GoogleAuth.isSignedIn.get()
         }).catch((error) => {
           console.error(error)
@@ -66,7 +69,7 @@ var googleAuth = (function () {
           reject(false)
           return
         }
-        this.GoogleAuth.signIn()
+        this.GoogleAuth.signIn({ux_mode: 'redirect', redirect_uri: ''})
           .then(googleUser => {
             if (typeof successCallback === 'function') successCallback(googleUser)
             this.isAuthorized = this.GoogleAuth.isSignedIn.get()
@@ -86,7 +89,7 @@ var googleAuth = (function () {
           reject(false)
           return
         }
-        this.GoogleAuth.grantOfflineAccess({ prompt: this.prompt })
+        this.GoogleAuth.grantOfflineAccess({ prompt: this.prompt, ux_mode: 'redirect' })
           .then(function (resp) {
             if (typeof successCallback === 'function') successCallback(resp.code)
             resolve(resp.code)
@@ -117,6 +120,35 @@ var googleAuth = (function () {
           })
       })
     };
+
+    this.setSigninStatus = (isSignedIn) => {
+      var user = this.GoogleAuth.currentUser.get();
+      var isAuthorized = user.hasGrantedScopes('profile email');
+      if (isAuthorized) {
+        return true;
+      } else {
+        return false
+      }
+
+    }
+    this.isSignedIn_ = (successCallback, errorCallback) => {
+      return new Promise((resolve, reject) => {
+        if (!this.GoogleAuth) {
+          if (typeof errorCallback === 'function') errorCallback(false)
+          this.isAuthorized = false
+          reject(false)
+          return
+        }
+        if (typeof successCallback === 'function') successCallback()
+        // let response = this.GoogleAuth.isSignedIn.listen(this.setSigninStatus())
+        let response = this.GoogleAuth.isSignedIn.get()
+        if(response) {
+          var googleUser = this.GoogleAuth.currentUser.get()
+          var isAuthorized = googleUser.hasGrantedScopes('profile email');
+          resolve(googleUser)
+        }
+      })
+    }
   }
 
   return new Auth()
@@ -129,14 +161,16 @@ function installGoogleAuthPlugin(Vue, options) {
   /* eslint-disable */
   //set config
   let GoogleAuthConfig = null
-  let GoogleAuthDefaultConfig = { scope: 'profile email', discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'] }
+  let GoogleAuthDefaultConfig = { scope: 'profile email', discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'], ux_mode: 'redirect' }
   let prompt = 'select_account'
   let ux_mode = 'redirect'
+  let redirect_uri = ''
   if (typeof options === 'object') {
     GoogleAuthConfig = Object.assign(GoogleAuthDefaultConfig, options)
     if (options.scope) GoogleAuthConfig.scope = options.scope
     if (options.prompt) prompt = options.prompt
     if (options.ux_mode) ux_mode	= options.ux_mode
+    if (options.redirect_uri) redirect_uri = options.redirect_uri
     if (!options.clientId) {
       console.warn('clientId is required')
     }
@@ -153,7 +187,7 @@ function installGoogleAuthPlugin(Vue, options) {
       }
     }
   })
-  Vue.gAuth.load(GoogleAuthConfig, prompt, ux_mode)
+  Vue.gAuth.load(GoogleAuthConfig, prompt, ux_mode, redirect_uri)
 }
 
 export default installGoogleAuthPlugin
